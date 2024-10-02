debd69559dd3800b83b189b16d1995705b2a433a
Samson Aderonmu <143148845+samsonHRC@users.noreply.github.com>
2024-10-02 11:09:06 +0100

    chore: Update Hardhat configuration files and dependencies

--- ignition/modules/NFT.ts
+++ ignition/modules/NFT.ts
@@ -1,14 +1,14 @@
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
-const Address = "address";
-
+const Address = "0xe47B685FD028eb63dD020E5641C5bD3Ce4adD610";
 
 const NftModule = buildModule("NftModule", (m) => {
   const owner = Address;
 
-
+  // Deploy the NFT contract with the owner address
   const Nft = m.contract("NFT", [owner]);
 
+  // Return the contract future as an object
   return { Nft };
 });
 
